aa3e5268227ceaf1ce87a8404410aa7fcb35444d
MikleJerry <just_foramoment_539@mail.ru>
2022-12-03 15:09:41 +0300

    second commit

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ def count_digits(line=''):
             digits_number += 1
     return digits_number
 
+
+def test_count_digits():
+    assert count_digits() == 0
+    assert count_digits('Без цифр!') == 0
+    assert count_digits('Привет, 0_0 мирР!?') == 2
